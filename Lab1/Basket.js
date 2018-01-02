5fb17303bd770fa5e312024795b15378974dbdc6
ElisaKu <35045190+ElisaKu@users.noreply.github.com>
2018-01-03 01:47:04 +0200

    Update Basket.js

--- Lab1/Basket.js
+++ Lab1/Basket.js
@@ -10,25 +10,60 @@ var prodcuts = [{
 
 class ProductLineItem {
 	constructor(product) {
-		//implement
+		this.name=product.name;
+		this.price=product.price;
+		this.inventory=1;
 	}
 }
 
 
 var basket = (function(){
-	//implement
+	var productArray=[];
 	return {
 		addProduct : function(productID){
-			//implement
+			if(!(productID in products)||products[productID].inventory == 0)
+				return false;
+			var itemIndex = this.getIdOf(products[productID].name);
+			if (itemIndex==-1)
+				productArray.push(new ProductLineItem(products[productID]))
+			else productArray[itemIndex].inventory--;
+			return true;
 		},
+		
 		removeProduct : function(productID){
-			//implement
+			if(!(productID in products)) 
+				return false;
+			var itemIndex = this.getidOf(products[productID].name);
+			if (itemIndex==-1) 
+				return false;
+			if(productArray[itmIndex].inventory>0) productArray[itmIndex].inventory--;
+			return true;
 		},
+										 
 		updateProductQuantity : function(productID, quantity) {
-			//implement
+			if(!(productID in products)) 
+				return false;
+			var itemIndex = this.getidOf(products[productID].name);
+			if (itemIndex == -1) 
+				return false;
+			productArray[itmIndex].inventory = quantity;
+			return true;
 		},
+			
 		getTotalPrice : function(){
-			//implement
+			var totalPrice=0;
+			for(var i = 0; i < productArray.length; i++){
+				totalPrice += productArray[i].price * productArray[i].inventory;
+			}
+			return totalPrice;
+		},
+		
+		getIdOf: function(productName){
+		for(var i = 0; i<productArray.length;i++){
+			if (productArray[i].name==productName)
+				return i;
+		}
+			return -1;
 		}
 	}
 })();
